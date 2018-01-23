0baad2a8f743444eeed5a256e4803f60883c99f7
Elliot Davis Greenlee <egreenle@com1707.eecs.utk.edu>
2018-01-23 16:37:09 -0500

    update linestring conversion

--- code/scoring/all_lines_to_images.sh
+++ code/scoring/all_lines_to_images.sh
@@ -1,7 +1,7 @@
-python linestring_conversion.py --image_directory ../../data/AOI_2_Vegas_Roads_Train/MUL-PanSharpen --csv_file ../../data/AOI_2_Vegas_Roads_Train/summaryData/AOI_2_Vegas_Roads_Train.csv --ground_directory ../../data/AOI_2_Vegas_Roads_Train/MUL-PanSharpen/ground_truth --width 1
+python linestring2mask.py --image_directory ../../data/AOI_2_Vegas_Roads_Train/MUL-PanSharpen --csv_file ../../data/AOI_2_Vegas_Roads_Train/summaryData/AOI_2_Vegas_Roads_Train.csv --ground_directory ../../data/AOI_2_Vegas_Roads_Train/MUL-PanSharpen/ground_truth --width 10
 
-python linestring_conversion.py --image_directory ../../data/AOI_3_Paris_Roads_Train/MUL-PanSharpen --csv_file ../../data/AOI_3_Paris_Roads_Train/summaryData/AOI_3_Paris_Roads_Train.csv --ground_directory ../../data/AOI_3_Paris_Roads_Train/MUL-PanSharpen/ground_truth --width 1
+python linestring2mask.py --image_directory ../../data/AOI_3_Paris_Roads_Train/MUL-PanSharpen --csv_file ../../data/AOI_3_Paris_Roads_Train/summaryData/AOI_3_Paris_Roads_Train.csv --ground_directory ../../data/AOI_3_Paris_Roads_Train/MUL-PanSharpen/ground_truth --width 10
 
-python linestring_conversion.py --image_directory ../../data/AOI_4_Shanghai_Roads_Train/MUL-PanSharpen --csv_file ../../data/AOI_4_Shanghai_Roads_Train/summaryData/AOI_4_Shanghai_Roads_Train.csv --ground_directory ../../data/AOI_4_Shanghai_Roads_Train/MUL-PanSharpen/ground_truth --width 1
+python linestring2mask.py --image_directory ../../data/AOI_4_Shanghai_Roads_Train/MUL-PanSharpen --csv_file ../../data/AOI_4_Shanghai_Roads_Train/summaryData/AOI_4_Shanghai_Roads_Train.csv --ground_directory ../../data/AOI_4_Shanghai_Roads_Train/MUL-PanSharpen/ground_truth --width 10
 
-python linestring_conversion.py --image_directory ../../data/AOI_5_Khartoum_Roads_Train/MUL-PanSharpen --csv_file ../../data/AOI_5_Khartoum_Roads_Train/summaryData/AOI_5_Khartoum_Roads_Train.csv --ground_directory ../../data/AOI_5_Khartoum_Roads_Train/MUL-PanSharpen/ground_truth --width 1
+python linestring2mask.py --image_directory ../../data/AOI_5_Khartoum_Roads_Train/MUL-PanSharpen --csv_file ../../data/AOI_5_Khartoum_Roads_Train/summaryData/AOI_5_Khartoum_Roads_Train.csv --ground_directory ../../data/AOI_5_Khartoum_Roads_Train/MUL-PanSharpen/ground_truth --width 10
